6177cb9dc457a2c48ee8cdcd169d3b22f1c74248
Sahaj9238 <sahajparikh2007@gmail.com>
2025-11-30 19:18:57 +0530

    Create day38q76.c

---
+++ day38/day38q76.c
@@ -0,0 +1,37 @@
+/*Q76: Check if a matrix is symmetric.
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+2 1
+Output 1:
+True
+Input 2:
+2 2
+1 0
+2 1
+Output 2:
+False*/
+int m,n;
+    printf("enter the no of rows in matrix A:");
+    scanf("%d",&m);
+    printf("enter the no of columns in matrix A:");
+    scanf("%d",&n);
+    if(m!=n)
+    {
+        printf("To be a symmetric matrix , the matrix should be square matrix first ");
+        return 1;
+    }
+    for (i=0;i<m;i++)
+    {
+        for (j=0;j<n;j++){
+            if (x[i][j]!=x[j][i])
+            {
+                printf("False");
+                return 0;
+            }
+        }
+    }
+    printf("True");
+    return 0;
+}
